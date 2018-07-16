d0da821b5271b0c77b81ee698e2106d01da5a163
Admin <Admin@X>
2018-07-17 00:34:16 +0300

    Add project Shop of fridges

--- ShopFridgeADO.NET/Form1.Designer.cs
+++ ShopFridgeADO.NET/Form1.Designer.cs
@@ -28,12 +28,242 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lbNameSaller = new System.Windows.Forms.Label();
+            this.txbSaller = new System.Windows.Forms.TextBox();
+            this.txbCustomer = new System.Windows.Forms.TextBox();
+            this.lbNameBuyer = new System.Windows.Forms.Label();
+            this.lbProduct = new System.Windows.Forms.Label();
+            this.txbCountProduct = new System.Windows.Forms.TextBox();
+            this.lbCountProduct = new System.Windows.Forms.Label();
+            this.listBoxProduct = new System.Windows.Forms.ListBox();
+            this.btnAddToTable = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnLoadToDB = new System.Windows.Forms.Button();
+            this.btnShowProducts = new System.Windows.Forms.Button();
+            this.btnShowReceipt = new System.Windows.Forms.Button();
+            this.btnShowSales = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnLoadData = new System.Windows.Forms.Button();
+            this.btnShowBSaller = new System.Windows.Forms.Button();
+            this.btnShowBCustomer = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lbNameSaller
+            // 
+            this.lbNameSaller.AutoSize = true;
+            this.lbNameSaller.Location = new System.Drawing.Point(5, 30);
+            this.lbNameSaller.Name = "lbNameSaller";
+            this.lbNameSaller.Size = new System.Drawing.Size(87, 13);
+            this.lbNameSaller.TabIndex = 0;
+            this.lbNameSaller.Text = "ФИО Продавец";
+            // 
+            // txbSaller
+            // 
+            this.txbSaller.Location = new System.Drawing.Point(103, 27);
+            this.txbSaller.Name = "txbSaller";
+            this.txbSaller.Size = new System.Drawing.Size(120, 20);
+            this.txbSaller.TabIndex = 1;
+            // 
+            // txbCustomer
+            // 
+            this.txbCustomer.Location = new System.Drawing.Point(103, 63);
+            this.txbCustomer.Name = "txbCustomer";
+            this.txbCustomer.Size = new System.Drawing.Size(120, 20);
+            this.txbCustomer.TabIndex = 3;
+            // 
+            // lbNameBuyer
+            // 
+            this.lbNameBuyer.AutoSize = true;
+            this.lbNameBuyer.Location = new System.Drawing.Point(4, 66);
+            this.lbNameBuyer.Name = "lbNameBuyer";
+            this.lbNameBuyer.Size = new System.Drawing.Size(97, 13);
+            this.lbNameBuyer.TabIndex = 2;
+            this.lbNameBuyer.Text = "ФИО Покупатель";
+            // 
+            // lbProduct
+            // 
+            this.lbProduct.AutoSize = true;
+            this.lbProduct.Location = new System.Drawing.Point(5, 101);
+            this.lbProduct.Name = "lbProduct";
+            this.lbProduct.Size = new System.Drawing.Size(62, 13);
+            this.lbProduct.TabIndex = 4;
+            this.lbProduct.Text = "Продукция";
+            // 
+            // txbCountProduct
+            // 
+            this.txbCountProduct.Location = new System.Drawing.Point(103, 213);
+            this.txbCountProduct.Name = "txbCountProduct";
+            this.txbCountProduct.Size = new System.Drawing.Size(121, 20);
+            this.txbCountProduct.TabIndex = 7;
+            // 
+            // lbCountProduct
+            // 
+            this.lbCountProduct.AutoSize = true;
+            this.lbCountProduct.Location = new System.Drawing.Point(6, 216);
+            this.lbCountProduct.Name = "lbCountProduct";
+            this.lbCountProduct.Size = new System.Drawing.Size(66, 13);
+            this.lbCountProduct.TabIndex = 6;
+            this.lbCountProduct.Text = "Количество";
+            // 
+            // listBoxProduct
+            // 
+            this.listBoxProduct.FormattingEnabled = true;
+            this.listBoxProduct.Location = new System.Drawing.Point(103, 101);
+            this.listBoxProduct.Name = "listBoxProduct";
+            this.listBoxProduct.Size = new System.Drawing.Size(120, 95);
+            this.listBoxProduct.TabIndex = 8;
+            // 
+            // btnAddToTable
+            // 
+            this.btnAddToTable.Location = new System.Drawing.Point(9, 247);
+            this.btnAddToTable.Name = "btnAddToTable";
+            this.btnAddToTable.Size = new System.Drawing.Size(107, 31);
+            this.btnAddToTable.TabIndex = 9;
+            this.btnAddToTable.Text = "Add to Table";
+            this.btnAddToTable.UseVisualStyleBackColor = true;
+            this.btnAddToTable.Click += new System.EventHandler(this.btnAddToTable_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(249, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(468, 258);
+            this.dataGridView1.TabIndex = 10;
+            // 
+            // btnLoadToDB
+            // 
+            this.btnLoadToDB.Location = new System.Drawing.Point(122, 247);
+            this.btnLoadToDB.Name = "btnLoadToDB";
+            this.btnLoadToDB.Size = new System.Drawing.Size(103, 31);
+            this.btnLoadToDB.TabIndex = 16;
+            this.btnLoadToDB.Text = "Load to DB";
+            this.btnLoadToDB.UseVisualStyleBackColor = true;
+            this.btnLoadToDB.Click += new System.EventHandler(this.btnLoadToDB_Click);
+            // 
+            // btnShowProducts
+            // 
+            this.btnShowProducts.Location = new System.Drawing.Point(249, 288);
+            this.btnShowProducts.Name = "btnShowProducts";
+            this.btnShowProducts.Size = new System.Drawing.Size(95, 31);
+            this.btnShowProducts.TabIndex = 17;
+            this.btnShowProducts.Text = "Show Products";
+            this.btnShowProducts.UseVisualStyleBackColor = true;
+            this.btnShowProducts.Click += new System.EventHandler(this.btnShowProducts_Click);
+            // 
+            // btnShowReceipt
+            // 
+            this.btnShowReceipt.Location = new System.Drawing.Point(350, 288);
+            this.btnShowReceipt.Name = "btnShowReceipt";
+            this.btnShowReceipt.Size = new System.Drawing.Size(92, 31);
+            this.btnShowReceipt.TabIndex = 18;
+            this.btnShowReceipt.Text = "ShowReseipt";
+            this.btnShowReceipt.UseVisualStyleBackColor = true;
+            this.btnShowReceipt.Click += new System.EventHandler(this.btnShowReceipt_Click);
+            // 
+            // btnShowSales
+            // 
+            this.btnShowSales.Location = new System.Drawing.Point(448, 288);
+            this.btnShowSales.Name = "btnShowSales";
+            this.btnShowSales.Size = new System.Drawing.Size(81, 31);
+            this.btnShowSales.TabIndex = 19;
+            this.btnShowSales.Text = "Show Sales";
+            this.btnShowSales.UseVisualStyleBackColor = true;
+            this.btnShowSales.Click += new System.EventHandler(this.btnShowSales_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txbSaller);
+            this.groupBox1.Controls.Add(this.lbNameSaller);
+            this.groupBox1.Controls.Add(this.lbNameBuyer);
+            this.groupBox1.Controls.Add(this.txbCustomer);
+            this.groupBox1.Controls.Add(this.btnLoadToDB);
+            this.groupBox1.Controls.Add(this.lbProduct);
+            this.groupBox1.Controls.Add(this.listBoxProduct);
+            this.groupBox1.Controls.Add(this.btnAddToTable);
+            this.groupBox1.Controls.Add(this.lbCountProduct);
+            this.groupBox1.Controls.Add(this.txbCountProduct);
+            this.groupBox1.Location = new System.Drawing.Point(12, 41);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(231, 291);
+            this.groupBox1.TabIndex = 21;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // btnLoadData
+            // 
+            this.btnLoadData.Location = new System.Drawing.Point(11, 12);
+            this.btnLoadData.Name = "btnLoadData";
+            this.btnLoadData.Size = new System.Drawing.Size(224, 23);
+            this.btnLoadData.TabIndex = 22;
+            this.btnLoadData.Text = "Connect to DB";
+            this.btnLoadData.UseVisualStyleBackColor = true;
+            this.btnLoadData.Click += new System.EventHandler(this.btnLoadData_Click);
+            // 
+            // btnShowBSaller
+            // 
+            this.btnShowBSaller.Location = new System.Drawing.Point(535, 288);
+            this.btnShowBSaller.Name = "btnShowBSaller";
+            this.btnShowBSaller.Size = new System.Drawing.Size(87, 31);
+            this.btnShowBSaller.TabIndex = 23;
+            this.btnShowBSaller.Text = "ShowSaller";
+            this.btnShowBSaller.UseVisualStyleBackColor = true;
+            this.btnShowBSaller.Click += new System.EventHandler(this.btnShowBSaller_Click);
+            // 
+            // btnShowBCustomer
+            // 
+            this.btnShowBCustomer.Location = new System.Drawing.Point(628, 288);
+            this.btnShowBCustomer.Name = "btnShowBCustomer";
+            this.btnShowBCustomer.Size = new System.Drawing.Size(87, 31);
+            this.btnShowBCustomer.TabIndex = 24;
+            this.btnShowBCustomer.Text = "ShowCustomer";
+            this.btnShowBCustomer.UseVisualStyleBackColor = true;
+            this.btnShowBCustomer.Click += new System.EventHandler(this.btnShowBCustomer_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(729, 335);
+            this.Controls.Add(this.btnShowBCustomer);
+            this.Controls.Add(this.btnShowBSaller);
+            this.Controls.Add(this.btnLoadData);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnShowSales);
+            this.Controls.Add(this.btnShowReceipt);
+            this.Controls.Add(this.btnShowProducts);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Shop of fridges";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lbNameSaller;
+        private System.Windows.Forms.TextBox txbSaller;
+        private System.Windows.Forms.TextBox txbCustomer;
+        private System.Windows.Forms.Label lbNameBuyer;
+        private System.Windows.Forms.Label lbProduct;
+        private System.Windows.Forms.TextBox txbCountProduct;
+        private System.Windows.Forms.Label lbCountProduct;
+        private System.Windows.Forms.ListBox listBoxProduct;
+        private System.Windows.Forms.Button btnAddToTable;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnLoadToDB;
+        private System.Windows.Forms.Button btnShowProducts;
+        private System.Windows.Forms.Button btnShowReceipt;
+        private System.Windows.Forms.Button btnShowSales;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnLoadData;
+        private System.Windows.Forms.Button btnShowBSaller;
+        private System.Windows.Forms.Button btnShowBCustomer;
     }
 }
 
--- ShopFridgeADO.NET/Form1.cs
+++ ShopFridgeADO.NET/Form1.cs
@@ -1,20 +1,300 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Configuration;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
 namespace ShopFridgeADO.NET
 {
     public partial class Form1 : Form
     {
+        private int NumberCheck { get; set; } = -1;
+        private string NameSaller { get; set; } = null;
+        private string NameBuyer { get; set; } = null;
+        private string Product { get; set; } = null;
+        private string CountProduct { get; set; } = null;
+        List<int> Receipt = new List<int>();
+        List<int> Saled = new List<int>();
+        List<int> Buyed = new List<int>();
+        List<List<string>> ListCreatingTable = new List<List<string>>();
+        //private string[] tempMas = new string[6];
+        //private List<string> tempMas = new List<string>();
+
+        private string connectionString = null;
+        private SqlConnection conn = null;
+        private SqlDataAdapter dAdapter = null;
+        private DataSet dataset = null;
+        private SqlCommandBuilder commBuild = null;
+        private DataTable dtProduct;
+        private DataTable dtProductReceipt;
+        private DataTable dtProductBalanceSaller;
+        private DataTable dtProductBalanceCustomers;
+        private DataTable dtSales;
+        private DataTable CreatingTable;
+
+
+        private string partQuerySales = @"insert into Sales(IdProduct, NumberCheck, DateSale, NameSaller, NameBuyer, CountPurchase) values (@idprod, @numbch, @date, @names, @nameb, @count)";
+        private string partQueryProductBalanceSaller = @"insert into ProductBalance(IdProduct, Balance, DateProductBalance) values (@idprodbs, @cols, @dates)";
+        private string partQueryProductBalanceCustomer = @"insert into ProductBalanceCustomers(IdProduct, BalanceCustomers, DateBalanceCustomers) values (@idprodbc, @colc, @datec)";
+
+
         public Form1()
         {
             InitializeComponent();
+            Load += Form1_Load;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            connectionString = ConfigurationManager.ConnectionStrings["ConnectionShopFriedges"].ConnectionString;
+            conn = new SqlConnection();
+            dAdapter = new SqlDataAdapter();
+            conn.ConnectionString = connectionString;
+            CreatingTable = new DataTable();
+            CreatingTable.Columns.Add("IdProduct");
+            CreatingTable.Columns.Add("NumberCheck");
+            CreatingTable.Columns.Add("Date");
+            CreatingTable.Columns.Add("Saller");
+            CreatingTable.Columns.Add("Buyer");
+            CreatingTable.Columns.Add("Count");
+        }
+
+        // Загрузка данных в DataSet
+        private void btnLoadData_Click(object sender, EventArgs e)
+        {
+            string sql = null;
+            // проверка на наличие записей в таблице продажи, если есть взять последнее значение,private string NameBuyer { get; set; } если нет присвоить 1
+            try
+            {
+                //SqlConnection conn = new SqlConnection(connectionString);
+                dataset = new DataSet();
+                sql = "select * from Product; select IdProduct, sum(CountProduct) as 'CountReceipt' from ProductReceipt group by IdProduct; select IdProduct, sum(CountPurchase) as CountPurchase, NumberCheck from Sales group by IdProduct, NumberCheck; select IdProduct, sum(Balance) as CountSaller from ProductBalance group by IdProduct; select IdProduct, sum(BalanceCustomers) as CountCustomer from ProductBalanceCustomers group by IdProduct";
+                dAdapter = new SqlDataAdapter(sql, conn);
+                dataGridView1.DataSource = null;
+                commBuild = new SqlCommandBuilder(dAdapter);
+                dAdapter.Fill(dataset);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"{ex.Message}", "Error");
+            }
+            finally
+            {
+            }
+
+            dtProduct = dataset.Tables[0];
+            dtProductReceipt = dataset.Tables[1];
+            if (dataset.Tables[2].Rows.Count > 0)
+                dtSales = dataset.Tables[2];
+
+            if (dataset.Tables[3].Rows.Count > 0)
+                dtProductBalanceSaller = dataset.Tables[3];
+
+            if (dataset.Tables[4].Rows.Count > 0)
+                dtProductBalanceCustomers = dataset.Tables[4];
+
+            if (listBoxProduct.Items.Count != 0)
+                listBoxProduct.Items.Clear();
+            for (int i = 0; i < dtProduct.Rows.Count; i++)
+            {
+                listBoxProduct.Items.Add(dtProduct.Rows[i][2].ToString());
+            }
+        }
+
+
+
+        private void btnShowProducts_Click(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = null;
+            dataGridView1.DataSource = dataset.Tables[0];
+        }
+
+
+
+        private void btnShowReceipt_Click(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = null;
+            dataGridView1.DataSource = dataset.Tables[1];
+        }
+
+
+
+        private void btnLoadToDB_Click(object sender, EventArgs e)
+        {
+            bool flagRollback = false;
+            conn = new SqlConnection(connectionString);
+            SqlCommand comm = conn.CreateCommand();
+            SqlTransaction tran = null;
+            try
+            {
+                conn.Open();
+                //tran = conn.BeginTransaction();
+                ////comm = conn.CreateCommand();
+                //comm.Transaction = tran;
+
+                for (int i = 0; i < ListCreatingTable.Count; i++)
+                {
+                    tran = conn.BeginTransaction();
+                    //comm = conn.CreateCommand();
+                    comm.Transaction = tran;
+
+                    comm.CommandText = partQuerySales;
+                    comm.Parameters.AddWithValue("@idprod", ListCreatingTable[i][0].ToString());
+                    comm.Parameters.AddWithValue("@numbch", ListCreatingTable[i][1].ToString());
+                    comm.Parameters.AddWithValue("@date", ListCreatingTable[i][2].ToString());
+                    comm.Parameters.AddWithValue("@names", ListCreatingTable[i][3].ToString());
+                    comm.Parameters.AddWithValue("@nameb", ListCreatingTable[i][4].ToString());
+                    comm.Parameters.AddWithValue("@count", ListCreatingTable[i][5].ToString());
+                    comm.ExecuteNonQuery();
+
+                    comm.CommandText = partQueryProductBalanceSaller;
+                    comm.Parameters.AddWithValue("@idprodbs", ListCreatingTable[i][0].ToString());
+                    comm.Parameters.AddWithValue("@cols", ListCreatingTable[i][5].ToString());
+                    comm.Parameters.AddWithValue("@dates", ListCreatingTable[i][2].ToString());
+                    comm.ExecuteNonQuery();
+
+                    comm.CommandText = partQueryProductBalanceCustomer;
+                    comm.Parameters.AddWithValue("@idprodbc", ListCreatingTable[i][0].ToString());
+                    comm.Parameters.AddWithValue("@colc", ListCreatingTable[i][5].ToString());
+                    comm.Parameters.AddWithValue("@datec", ListCreatingTable[i][2].ToString());
+                    comm.ExecuteNonQuery();
+
+                    int sum = 0;
+                    for (int j = 0; j < dtProductReceipt.Rows.Count; j++)
+                    {
+                        for (int k = 0; k < CreatingTable.Rows.Count; k++)
+                        {
+                            if (dtProductReceipt.Rows[j][0].ToString() == CreatingTable.Rows[k][0].ToString())
+                                sum += Int32.Parse(CreatingTable.Rows[k][5].ToString());
+                        }
+                        if (dtProductBalanceSaller.Rows.Count != 0)
+                        {
+                            if (dtProductBalanceSaller.Rows.Count > j)
+                            {
+                                if (dtProductReceipt.Rows[j][0].ToString() == dtProductBalanceSaller.Rows[j][0].ToString())
+                                {
+                                    int tempRes = Int32.Parse(dtProductReceipt.Rows[j][1].ToString()) - Int32.Parse(dtProductBalanceSaller.Rows[j][1].ToString());
+                                    if (tempRes < sum)
+                                    {
+                                        tran.Rollback();
+                                        return;
+                                    }
+                                }
+                            }
+                        }
+                        sum = 0;
+                    }
+                    comm.Parameters.Clear();
+                    tran.Commit();
+                }
+                //tran.Commit();
+                CreatingTable.Clear();
+                CreatingTable = null;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                tran.Rollback();
+                CreatingTable.Clear();
+                CreatingTable = null;
+            }
+            finally
+            {
+                //CreatingTable.Clear();
+                CreatingTable = null;
+                conn.Close();
+            }
+        }
+
+
+
+        private void btnAddToTable_Click(object sender, EventArgs e)
+        {
+            List<string> tempMas = new List<string>();
+            if (dtSales == null)
+                NumberCheck = 1;
+
+            if (dtSales != null)
+            {
+                if (CreatingTable.Rows.Count != 0)
+                    NumberCheck = Int32.Parse(CreatingTable.Rows[CreatingTable.Rows.Count - 1][1].ToString());
+                if (CreatingTable.Rows.Count == 0)
+                {
+                    int res = 0;
+                    bool isInt = Int32.TryParse(dtSales.Rows[dtSales.Rows.Count - 1][1].ToString(), out res);
+                    if (isInt)
+                        NumberCheck = res + 1;
+                }
+            }
+            for (int i = 0; i < dtProduct.Rows.Count; i++)
+            {
+                if (dtProduct.Rows[i][2].ToString() == listBoxProduct.SelectedItem.ToString())
+                    Product = dtProduct.Rows[i][0].ToString();
+            }
+            NameSaller = txbSaller.Text;
+            NameBuyer = txbCustomer.Text;
+            CountProduct = txbCountProduct.Text;
+            tempMas.Add(Product);
+            tempMas.Add(NumberCheck.ToString());
+            tempMas.Add(DateTime.Now.ToString("yyyy-MM-dd hh:mm:ss tt"));
+            tempMas.Add(NameSaller);
+            tempMas.Add(NameBuyer);
+            tempMas.Add(CountProduct);
+
+            //StringBuilder strB = new StringBuilder();
+            //strB.Append(Product);
+            //tempMas[0] = strB.ToString();
+            //strB.Clear();
+            //strB.Append(NumberCheck.ToString());
+            //tempMas[1] = strB.ToString();
+            //strB.Clear();
+            //strB.Append(DateTime.Now.ToString("yyyy-MM-dd hh:mm:ss tt"));
+            //tempMas[2] = strB.ToString();
+            //strB.Clear();
+            //strB.Append(NameSaller);
+            //tempMas[3] = strB.ToString();
+            //strB.Clear();
+            //strB.Append(NameBuyer);
+            //tempMas[4] = strB.ToString();
+            //strB.Clear();
+            //strB.Append(CountProduct);
+            //tempMas[5] = strB.ToString();
+            ListCreatingTable.Add(tempMas);
+            
+            DataRow newdrow = CreatingTable.NewRow();
+            newdrow[0] = Product;
+            newdrow[1] = NumberCheck.ToString();
+            newdrow[2] = DateTime.Now.ToString("yyyy-MM-dd hh:mm:ss tt");
+            newdrow[3] = NameSaller;
+            newdrow[4] = NameBuyer;
+            newdrow[5] = CountProduct;
+            CreatingTable.Rows.Add(newdrow);
+            dataGridView1.DataSource = CreatingTable;
+        }
+
+        private void btnShowSales_Click(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = null;
+            dataGridView1.DataSource = dataset.Tables[2];
+        }
+
+        private void btnShowBSaller_Click(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = null;
+            dataGridView1.DataSource = dataset.Tables[3];
+        }
+
+        private void btnShowBCustomer_Click(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = null;
+            dataGridView1.DataSource = dataset.Tables[4];
         }
     }
 }
